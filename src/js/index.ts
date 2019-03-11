d5eb95526742eaafb6a8d34b482e8564238a10f2
Jacob Larsen <captainpowerturtle@gmail.com>
2019-03-11 13:17:23 +0100

    Havde glemt det

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,19 @@
-//Her skal du skrive din typescriptkode
+let e1 = document.getElementById("0");
+let e2 = document.getElementsByClassName("usundt");
+let e3 = document.getElementsByTagName("li");
+console.log(e1);
+console.log(e2);
+console.log(e3);
+
+let n1 = e1.setAttribute("hidden","");
+
+for (let index = 0; index < e2.length; index++) {
+    const element = e2[index];
+    element.setAttribute("class","sødt")
+    console.log(element)
+}
+
+let e4 = e1.parentNode.nodeName;
+console.log(e4)
+let e5 = e1.firstChild
+console.log(e5)
